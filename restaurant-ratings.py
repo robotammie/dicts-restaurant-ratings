45a6827688586470da3008ecc288e7011dc1246b
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 16:28:30 -0700

    implemented command-line argument

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,17 +1,23 @@
-scores_file = open('scores.txt')
+import sys
 
 r_scores = {}
 
-# populate dictionary of restaurants and their scores
-for line in scores_file:
-    line = line.rstrip()
-    tokens = line.split(':')
+if len(sys.argv) != 2:
+    print "ERROR: function requires a single file name to pull restaurant data from."
 
-    r_scores[tokens[0]] = tokens [1]
+else:
+    scores_file = open(sys.argv[1])
 
-# create sorted list from dictionary
-r_scores_alpha = sorted(r_scores.items())
+    # populate dictionary of restaurants and their scores
+    for line in scores_file:
+        line = line.rstrip()
+        tokens = line.split(':')
 
-# iterate over the sorted list and print each restaurant and its rating
-for restaurant in r_scores_alpha:
-    print restaurant[0], "is rated at", restaurant[1]
+        r_scores[tokens[0]] = tokens [1]
+
+    # create sorted list from dictionary
+    r_scores_alpha = sorted(r_scores.items())
+
+    # iterate over the sorted list and print each restaurant and its rating
+    for restaurant in r_scores_alpha:
+        print restaurant[0], "is rated at", restaurant[1]
