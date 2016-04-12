11eb5ed552e8e755c68715e56f7772eb6bd97a9b
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:55:25 -0700

    implimented user input opton for additional data

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,11 +1,12 @@
 import sys
 
-r_scores = {}
 
 if len(sys.argv) != 2:
     print "ERROR: function requires a single file name to pull restaurant data from."
 
 else:
+    r_scores = {}
+
     scores_file = open(sys.argv[1])
 
     # populate dictionary of restaurants and their scores
@@ -14,6 +15,19 @@ else:
         tokens = line.split(':')
 
         r_scores[tokens[0]] = tokens [1]
+
+    # request user input
+    while True:
+        user_input = raw_input("Add additional restaurants? y/n ")
+        
+        if user_input != "y":
+            break
+        
+        restaurant = raw_input("Restaurant Name: ")
+        rating = raw_input("Rating: ")
+
+        r_scores[restaurant] = rating
+
 
     # create sorted list from dictionary
     r_scores_alpha = sorted(r_scores.items())
